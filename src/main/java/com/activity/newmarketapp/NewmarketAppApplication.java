e818ae8c6150a9a181b292ba8b1d43606a0b7190
NathanSoa <nathansoares809@gmail.com>
2022-12-22 13:52:28 -0300

    initial commit

---
+++ src/main/java/com/activity/newmarketapp/NewmarketAppApplication.java
@@ -0,0 +1,13 @@
+package com.activity.newmarketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewmarketAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewmarketAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/activity/newmarketapp/NewmarketAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activity.newmarketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewmarketAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
