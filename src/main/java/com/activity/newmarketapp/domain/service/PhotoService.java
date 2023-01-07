f893880be969699bb92911096902323a5c17dc63
NathanSoa <nathansoares809@gmail.com>
2023-01-07 19:31:32 -0300

    refactor: updated method to create the product object properly

--- src/main/java/com/activity/newmarketapp/domain/service/PhotoService.java
+++ src/main/java/com/activity/newmarketapp/domain/service/PhotoService.java
@@ -3,6 +3,8 @@ package com.activity.newmarketapp.domain.service;
 import com.activity.newmarketapp.data.entities.Product;
 
 import com.activity.newmarketapp.data.repository.ProductRepository;
+import com.activity.newmarketapp.domain.mapper.ProductResponseMapper;
+import com.activity.newmarketapp.presentation.dtos.ProductResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,12 +16,14 @@ import java.io.IOException;
 public class PhotoService {
 
     private final ProductService productService;
+    private final ProductResponseMapper responseMapper;
     private final ProductRepository repository;
     @Value("${project.images.directory}")
     private String imageDirectory;
 
-    public PhotoService(ProductService service, ProductRepository repository) {
+    public PhotoService(ProductService service, ProductResponseMapper responseMapper, ProductRepository repository) {
         this.productService = service;
+        this.responseMapper = responseMapper;
         this.repository = repository;
     }
 
@@ -27,9 +31,10 @@ public class PhotoService {
         try{
             String imagePath = imageDirectory + multipartFile.getOriginalFilename();
             multipartFile.transferTo(new File(imagePath));
-            Product product = productService.findById(id);
-            product.setPhoto(imagePath);
-            repository.save(product);
+            ProductResponse product = productService.findById(id);
+            Product productWithPhoto = responseMapper.toEntity(product);
+            productWithPhoto.setPhoto(imagePath);
+            repository.save(productWithPhoto);
             return "Photo successfully updated!";
         } catch (IOException e) {
             return "An error occurred!";
