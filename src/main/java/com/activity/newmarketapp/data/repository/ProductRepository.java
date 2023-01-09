4dee531012759e94a10e3879efa777229c610f6e
NathanSoa <nathansoares809@gmail.com>
2023-01-09 14:34:13 -0300

    feat: add method to find product by name

--- src/main/java/com/activity/newmarketapp/data/repository/ProductRepository.java
+++ src/main/java/com/activity/newmarketapp/data/repository/ProductRepository.java
@@ -5,7 +5,10 @@ import com.activity.newmarketapp.data.repository.product.ProductRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryQuery {
 
+    Optional<Product> findByName(String name);
 }
