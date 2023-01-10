9677b27237f3996be57e894cbef8cdf7e4a860db
NathanSoa <nathansoares809@gmail.com>
2023-01-10 15:38:43 -0300

    feat: create custom exception handler and error POJO

---
+++ src/main/java/com/activity/newmarketapp/config/exception/CustomExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.activity.newmarketapp.config.exception;
+
+import com.activity.newmarketapp.config.exception.customexceptions.EntityAlreadyExistsException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({EmptyResultDataAccessException.class})
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException e, WebRequest request) {
+        Error error = new Error("Empty result data access exception", e.getMessage());
+        return handleExceptionInternal(e, error, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({BadCredentialsException.class})
+    public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException e, WebRequest request) {
+        Error error = new Error("Bad credentials exception", e.getMessage());
+        return handleExceptionInternal(e, error, new HttpHeaders(), HttpStatus.UNAUTHORIZED, request);
+    }
+
+    @ExceptionHandler({EntityAlreadyExistsException.class})
+    public ResponseEntity<Object> handleEntityAlreadyExistsException(EntityAlreadyExistsException e, WebRequest request) {
+        Error error = new Error("Entity already exists exception", e.getMessage());
+        return handleExceptionInternal(e, error, new HttpHeaders(), HttpStatus.CONFLICT, request);
+    }
+}
