442708a9c18c6080ecf74161aa4daf4a2e339f19
NathanSoa <nathansoares809@gmail.com>
2023-01-10 18:15:36 -0300

    feat: create route and service to find all categories

--- src/main/java/com/activity/newmarketapp/domain/service/CategoryService.java
+++ src/main/java/com/activity/newmarketapp/domain/service/CategoryService.java
@@ -2,16 +2,26 @@ package com.activity.newmarketapp.domain.service;
 
 import com.activity.newmarketapp.data.entities.Category;
 import com.activity.newmarketapp.data.repository.CategoryRepository;
+import com.activity.newmarketapp.domain.mapper.CategoryDTOMapper;
 import com.activity.newmarketapp.presentation.dtos.category.CategoryCreateRequest;
 import com.activity.newmarketapp.config.exception.customexceptions.EntityAlreadyExistsException;
+import com.activity.newmarketapp.presentation.dtos.category.CategoryDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+    private final CategoryDTOMapper categoryDTOMapper;
+
+    public Set<CategoryDTO> findAll() {
+        return categoryRepository.findAll().stream().map(categoryDTOMapper::toDTO).collect(Collectors.toSet());
+    }
 
     public String save(CategoryCreateRequest categoryCreateRequest) {
 
--- src/main/java/com/activity/newmarketapp/presentation/controller/CategoryController.java
+++ src/main/java/com/activity/newmarketapp/presentation/controller/CategoryController.java
@@ -2,11 +2,17 @@ package com.activity.newmarketapp.presentation.controller;
 
 import com.activity.newmarketapp.domain.service.CategoryService;
 import com.activity.newmarketapp.presentation.dtos.category.CategoryCreateRequest;
+import com.activity.newmarketapp.presentation.dtos.category.CategoryDTO;
+
 import jakarta.validation.Valid;
+
 import lombok.AllArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping("/category")
@@ -15,6 +21,11 @@ import org.springframework.web.bind.annotation.*;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<Set<CategoryDTO>> findAll() {
+        return ResponseEntity.ok(categoryService.findAll());
+    }
 
     @PostMapping
     public ResponseEntity<String> save(@Valid @RequestBody CategoryCreateRequest categoryCreateRequest) {
