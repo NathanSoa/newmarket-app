1d4f46d3c76497ae929cecf84b12378b3ae5a1a3
NathanSoa <nathansoares809@gmail.com>
2023-01-10 15:24:02 -0300

    fix: changed spring stereotype

--- src/main/java/com/activity/newmarketapp/data/repository/UserRepository.java
+++ src/main/java/com/activity/newmarketapp/data/repository/UserRepository.java
@@ -2,11 +2,11 @@ package com.activity.newmarketapp.data.repository;
 
 import com.activity.newmarketapp.data.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Service
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 }
